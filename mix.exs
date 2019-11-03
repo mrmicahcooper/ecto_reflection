2bd493f51ef419733514642f96d64263ca7bce13
Micah Cooper <micah+github@mrmicahcooper.com>
2019-11-03 16:23:13 -0500

    Add ecto as dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule EctoReflection.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -18,11 +19,16 @@ defmodule EctoReflection.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto, "~> 3.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      test: ["test --trace"]
     ]
   end
 end
