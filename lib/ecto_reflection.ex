bc7621bf9084ce7b48627d5fc5844865557d890c
Micah Cooper <micah+github@mrmicahcooper.com>
2019-11-13 15:36:45 -0500

    Add specs

--- lib/ecto_reflection.ex
+++ lib/ecto_reflection.ex
@@ -15,22 +15,26 @@ defmodule EctoReflection do
     for module <- modules, defines_schema?(module), do: module
   end
 
+  @spec attributes(Ecto.Schema) :: list(atom())
   def attributes(module) do
     module.__struct__
     |> Map.keys()
     |> Kernel.--(~w[__meta__ __struct__]a)
   end
 
+  @spec attributes(Ecto.Schema, :binary) :: list(binary())
   def attributes(module, :binary) do
     module
     |> attributes()
     |> map(&to_string/1)
   end
 
+  @spec attribute?(Ecto.Schema, atom() | binary() ):: boolean()
   def attribute?(module, key) do
     to_string(key) in (attributes(module, :binary))
   end
 
+  @spec fields(Ecto.Schema) :: list(atom())
   def fields(module) do
     module.__struct__
     |> Map.keys()
@@ -39,19 +43,22 @@ defmodule EctoReflection do
     |> Kernel.--(module.__schema__(:embeds))
   end
 
+  @spec fields(Ecto.Schema, :binary) :: list(binary())
   def fields(module, :binary) do
     module
     |> fields()
     |> map(&to_string/1)
   end
 
+  @spec field?(Ecto.Schema, atom() | binary()) :: boolean()
   def field?(module, key) do
     to_string(key) in fields(module, :binary)
   end
 
-  def type(schema, key) when is_binary(key) do
-    if attribute?(schema, key) do
-      type(schema, String.to_atom(key))
+  @spec type(Ecto.Schema, atom() | binary()) :: {atom(), atom() | module()} | {atom(), module(), module() | [atom()] }
+  def type(module, key) when is_binary(key) do
+    if attribute?(module, key) do
+      type(module, String.to_atom(key))
     end
   end
 
@@ -67,76 +74,92 @@ defmodule EctoReflection do
     |> data_type(schema)
   end
 
+  @spec source_fields(Ecto.Schema) :: list(atom())
   def source_fields(module) do
     module.__schema__(:fields)
   end
 
+  @spec source_fields(Ecto.Schema, :binary) :: list(binary())
   def source_fields(module, :binary) do
     module
     |> source_fields()
     |> map(&to_string/1)
   end
 
+  @spec source_field?(Ecto.Schema, atom() | binary()) :: boolean()
   def source_field?(module, key) do
     to_string(key) in source_fields(module, :binary)
   end
 
+  @spec virtual_fields(Ecto.Schema) :: list(atom())
   def virtual_fields(module) do
     fields(module) -- source_fields(module)
   end
 
+  @spec virtual_fields(Ecto.Schema, :binary) :: list(binary())
   def virtual_fields(module, :binary) do
     module
     |> virtual_fields()
     |> map(&to_string/1)
   end
 
+  @spec virtual_field?(Ecto.Schema, atom() | binary()) :: boolean()
   def virtual_field?(module, key) do
     to_string(key) in virtual_fields(module, :binary)
   end
 
+  @spec associations(Ecto.Schema) :: list(atom())
   def associations(module) do
     module.__schema__(:associations)
   end
 
+  @spec associations(Ecto.Schema, :binary) :: list(binary())
   def associations(module, :binary) do
     module
     |> associations()
     |> map(&to_string/1)
   end
 
+  @spec association?(Ecto.Schema, atom() | binary()) :: boolean()
   def association?(module, key) do
     to_string(key) in associations(module, :binary)
   end
 
+  @spec embeds(Ecto.Schema) :: list(atom())
   def embeds(module) do
     module.__schema__(:embeds)
   end
 
+  @spec embeds(Ecto.Schema, :binary) :: list(binary())
   def embeds(module, :binary) do
     module
     |> embeds()
     |> map(&to_string/1)
   end
 
+  @spec embed?(Ecto.Schema, atom() | binary()) :: boolean()
   def embed?(module, key) do
     to_string(key) in embeds(module, :binary)
   end
 
+  @spec relationships(Ecto.Schema) :: list(atom())
   def relationships(module) do
     associations(module) ++ embeds(module)
   end
 
+  @spec relationships(Ecto.Schema, :binary) :: list(binary())
   def relationships(module, :binary) do
     module
     |> relationships()
     |> map(&to_string/1)
   end
 
+  @spec relationship?(Ecto.Schema, atom() | binary()) :: boolean()
   def relationship?(module, key) do
     to_string(key) in relationships(module, :binary)
   end
 
+  @spec source_schema(Ecto.Query) :: Ecto.Schema
   def source_schema(query) do
     query.from.source |> elem(1)
   end
